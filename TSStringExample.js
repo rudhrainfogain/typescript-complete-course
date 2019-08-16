2121c52fbb66a33bcbd9502ae079ddf2fe38b626
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2019-08-17 02:24:23 +0530

    Code checkin for following changes:- 1.Added ts autocompilation as a prestart step to package.json 2.Added strict type check comparison example

---
+++ TSStringExample.js
@@ -0,0 +1,9 @@
+"use strict";
+var myname = "Rudhra";
+//cannot assign a number to the variable myname as we are using let keyword to declare the variable and it infers it to be a string
+//myname = 22;
+var myname1 = "Rudhra";
+//cannot assign a number to the variable myname1 as typescript infers it to be a string
+//myname1=21;
+console.log("mynameis " + myname);
+console.log("myname1 is " + myname1);
---
+++ jsStringExample.js
@@ -0,0 +1,4 @@
+var myname2 = "Rudhra";
+//since we are using js which does not enforce type safety we can assign a number to a string
+myname2 = 22;
+console.log("myname2 is " + myname2);
